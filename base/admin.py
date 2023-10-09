d847d995fa38fa55953b01ae4ee8e5427d163a49
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2023-10-09 11:32:59 +0330

    Improve admin panel. Category model custom and registered

--- base/admin.py
+++ base/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from base.models import Post, Review, Reply
+from base.models import Post, Review, Reply, Category
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'create', 'status', 'tag_list', )
+    list_display = ('title', 'author', 'create', 'status', 'category', 'tag_list', )
     list_filter = ('status', 'create', 'author')
     ordering = ('status', 'publish')
     date_hierarchy = 'create'
     search_fields = ('title__startswith',)
+    prepopulated_fields = {"slug": ["title"]}
     
     def tag_list(self, obj):
         return u", ".join(o.name for o in obj.tags.all())
@@ -27,3 +28,9 @@ class ReviewAdmin(admin.ModelAdmin):
 @admin.register(Reply)
 class ReplyAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'create')
+
