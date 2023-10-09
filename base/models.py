13a619903c9b30bdf6428f9ddc95f8cd6c829deb
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2023-10-09 11:07:20 +0330

    Category model created. makemigrations and migrate Done

--- base/models.py
+++ base/models.py
@@ -5,16 +5,29 @@ from taggit.managers import TaggableManager
 from django.template.defaultfilters import slugify
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    create = models.DateTimeField(auto_now_add=True)
+        
+    def __str__(self):
+        return self.name
+    
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+        
+
 class Post(models.Model):
     STATUS_CHOICES = ((('draft', 'Draft'),('published', 'Published')))
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique_for_date='publish')
     body = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     create = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     publish = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='posts', null=True)
     tags = TaggableManager()
     
     def __str__(self):
